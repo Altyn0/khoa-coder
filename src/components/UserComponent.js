6b12a8f38a4cf88394a7099376feb4d2c17a34d4
Altyn0 <96158393+Altyn0@users.noreply.github.com>
2023-10-17 17:10:02 +0700

    create new app

---
+++ src/components/UserComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const usercomponent = () => {
+  return (
+    <div>
+        <h5>User name</h5>
+        <p>Age</p>
+        <p>address</p>
+    </div>
+  )
+}
+
+export default usercomponent
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { users } from '../data/Users'
+const HomeScreen = () => {
+    return <div>
+          <h3> {users[0].name} </h3> 
+            </div>
+}
+
+export default HomeScreen
