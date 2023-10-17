f601cc42be8e4b212e9821c7e89429a8720072e5
Altyn0 <96158393+Altyn0@users.noreply.github.com>
2023-10-17 17:13:57 +0700

    update user

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const usercomponent = () => {
   return (
-    <div>
+    <>
         <h5>User name</h5>
         <p>Age</p>
         <p>address</p>
-    </div>
+    </>
   )
 }
 
