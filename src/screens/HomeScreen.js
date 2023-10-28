643586a07973a8f64f379bc56cbfca3937a1b8b0
Altyn0 <96158393+Altyn0@users.noreply.github.com>
2023-10-28 18:53:18 +0700

    state

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,43 +1,13 @@
-import React from 'react'
-import { users } from '../data/Users'
-import UserComponent from '../components/UserComponent'
-import HeaderComponent from '../components/HeaderComponent';
-import TextComponent from '../components/TextComponent';
-import CardComponent from '../components/CardComponent';
+import React,{ useState } from 'react'
 const HomeScreen = () => {
-    const sayHi =() =>{
+   const [name, setName] = useState('');
+    return ( 
+    <div>
+      <p>{name}</p>
+    <button onClick={() => setName('Mindx')}> change name </button>
+    </div>
 
-      alert('Hello World!!!!');
-    }
-
-
-    return( <div> 
-      
-      <a onClick={sayHi}> Khoa đẹp trai  </a>
-
-      
-      </div>
-      
-    //   {/* /* <HeaderComponent/>
-
-    //  <CardComponent color='coral'>
-    //   <p>Profile Screen</p>
-    //     </CardComponent>
-
-    //  <CardComponent  color='coral'>
-    //   <p>HomeScreen</p>
-        
-
-    //   {users.map((user) =>
-    //     (<UserComponent 
-    //       key ={user.id} 
-    //       name={user.name} 
-    //       email={user.email} 
-    //       company={user.company} />
-    //   ))}
-    //   </CardComponent> */} 
-    
-     ) ;  
+     );  
 };
 
 export default HomeScreen
