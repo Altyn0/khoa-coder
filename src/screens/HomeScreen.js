d2ccb6e5740dba57d510c06f567876c4e431d335
Altyn0 <96158393+Altyn0@users.noreply.github.com>
2023-10-28 18:56:03 +0700

    state_1

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import React,{ useState } from 'react'
 const HomeScreen = () => {
-   const [name, setName] = useState('');
+   const [name, setName] = useState(0);
     return ( 
     <div>
       <p>{name}</p>
-    <button onClick={() => setName('Mindx')}> change name </button>
+    <button onClick={() => setName(Math.floor(Math.random()*100))}> change name </button>
     </div>
 
      );  
